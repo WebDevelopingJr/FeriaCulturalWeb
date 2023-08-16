51de23e89bab941b908bb4d25521f694de092a9d
Nestor Castillo <131619700+WebDevelopingJr@users.noreply.github.com>
2023-08-15 21:05:13 -0600

    Update index.js

--- index.js
+++ index.js
@@ -13,3 +13,35 @@ let menuScroll = document.querySelector("#nav-bar")
 window.addEventListener("scroll",(e)=>{
    menuScroll.classList.toggle("menuScroll", window.scrollY>0);
 });
+
+let cartasAdicionales2 = document.getElementById("cart2")
+let cartasAdicionales3 = document.getElementById("cart3")
+let cartasAdicionales4 = document.getElementById("cart4")
+
+let cartas = document.querySelector(".content") 
+cartas.addEventListener("click", ()=>{
+    cartas.classList.toggle("CartasInfo")
+    cartasAdicionales2.classList.remove("CartasInfo")
+    cartasAdicionales3.classList.remove("CartasInfo")
+    cartasAdicionales4.classList.remove("CartasInfo")
+});
+cartasAdicionales2.addEventListener("click", ()=>{
+    cartas.classList.remove("CartasInfo")
+    cartasAdicionales2.classList.toggle("CartasInfo")
+    cartasAdicionales3.classList.remove("CartasInfo")
+    cartasAdicionales4.classList.remove("CartasInfo")
+
+})
+cartasAdicionales3.addEventListener("click", ()=>{
+    cartas.classList.remove("CartasInfo")
+    cartasAdicionales3.classList.toggle('CartasInfo')
+    cartasAdicionales2.classList.remove("CartasInfo")
+    cartasAdicionales4.classList.remove("CartasInfo")
+})
+cartasAdicionales4.addEventListener("click",()=>{
+    cartas.classList.remove("CartasInfo")
+    cartasAdicionales4.classList.toggle("CartasInfo")
+    cartasAdicionales2.classList.remove("CartasInfo")
+    cartasAdicionales3.classList.remove("CartasInfo")
+
+})
